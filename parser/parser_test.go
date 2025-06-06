7b34d3c1bfc6ee7e0fb600720a92ce4ca95469d1
seblkma <sebmalikkeung@gmail.com>
2025-06-06 08:05:16 +0000

    added parser test

---
+++ parser/parser_test.go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/seblkma/go-himeji/ast"
+	"github.com/seblkma/go-himeji/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 20;
+	let y = 22;
+	let foobar = 838383;
+	`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil\n")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("Program statements expected %d, got %d\n", 3, len(program.Statements))
+	}
+	expectedIdentifiers := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, ei := range expectedIdentifiers {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, ei.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral expected 'let' but got=%s\n", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement) // type conversion, may or may not be LetStatement
+	if !ok {
+		t.Errorf("s expected LetStatement but got=%T\n", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value expected %s but got=%s\n", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() expected %s but got=%s\n", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
